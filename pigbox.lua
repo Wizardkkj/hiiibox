90693788f36a2b05be834e024b0150bef22ca689
lvtoness <88632356+Wizardkkj@users.noreply.github.com>
2023-08-21 08:03:06 -0300

    Update pigbox.lua

--- pigbox.lua
+++ pigbox.lua
@@ -1,10 +1,16 @@
 local part = game:GetService("Workspace").Challenge.PiggyPlaytime.PigZel.Union
 
+-- Variável para controlar se o objeto já teve a hitbox expandida
+local hitboxExpanded = false
+
 -- Função para expandir a hitbox
 local function expandHitbox(object, amount)
-    local originalSize = object.Size
-    local newSize = originalSize + Vector3.new(amount, amount, amount)
-    object.Size = newSize
+    if not hitboxExpanded then
+        local originalSize = object.Size
+        local newSize = originalSize + Vector3.new(amount, amount, amount)
+        object.Size = newSize
+        hitboxExpanded = true
+    end
 end
 
 -- Função para desenhar a hitbox
@@ -19,16 +25,25 @@ local function onTouched(otherPart)
     local character = otherPart.Parent
     if character and character:FindFirstChild("Humanoid") then
         local humanoid = character:FindFirstChild("Humanoid")
-        -- Aqui você pode adicionar a lógica para lidar com a interação
+        -- Adicione um print para mostrar que o toque ocorreu
         print(character.Name .. " tocou no objeto com hitbox expandida!")
+        -- Aqui você pode ajustar a lógica de movimento do personagem após a interação
     end
 end
 
 -- Configure a função onTouched para o objeto
 part.Touched:Connect(onTouched)
 
--- Chame a função para expandir a hitbox do objeto em 10 unidades
+-- Chame a função para expandir a hitbox do objeto em 10 unidades (se ainda não foi expandida)
 expandHitbox(part, 10)
 
 -- Chame a função para desenhar a hitbox do objeto
 drawHitbox(part)
+
+-- Loop para executar o script várias vezes
+while true do
+    wait(1) -- Aguarde 1 segundo entre as execuções do loop
+    
+    -- Aqui você pode adicionar qualquer lógica adicional que desejar executar no loop
+    print("Script sendo executado no loop")
+end
